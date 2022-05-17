382532f91febcf07e39fe2a1e0f556a1590f8975
caioems <caioeduardoms@hotmail.com>
2022-05-17 17:54:47 -0300

    Update Log_Analyzer.py

--- Log_Analyzer.py
+++ Log_Analyzer.py
@@ -2,6 +2,9 @@
 """
 Created on Thu Apr 21 17:17:20 2022
 
+Script designed to take useful information from BIN dataflash logs and
+present it on a KML file.
+
 @author: [t2]caiera
 """
 
@@ -19,40 +22,21 @@ root.update()
 path = askdirectory(title='Select the root folder:')
 root.destroy()
 
-types = ["CAM", "CURR", "ERR"] #EV, GPS (HDop), MSG, PARM, POWR, RCOU
-
 def create_log_path (root_path):
     log_list = list(pathlib.Path(root_path).glob(r"**\**\*.BIN"))
     return log_list   
 
 #importing data
 def create_csv(log_path):
+    types = ["CAM", "EV"] #BAT, EV, ERR, GPS (HDop), MSG, PARM, POWR, RCOU
     log = log_path.as_posix()
     path = log_path.parent    
-    for i in types:
-        mycmd = "mavlogdump.py --planner --format csv --types " + i + " " + str(log) + " > " + str(path) + "/" + i + ".csv"
+    for t in types:
+        mycmd = "mavlogdump.py --planner --format csv --types " + t + " " + str(log) + " > " + str(path) + "/" + t + ".csv"
         os.system(mycmd)
 
-def create_cam_df(log_path):
-    file = "CAM.csv"
-    location = log_path.parent
-    csv_file = os.path.join(location, file)
-    df = pd.read_csv(csv_file, index_col='timestamp')
-    df.index = pd.to_datetime(df.index, unit='s', origin='unix')
-    os.remove(csv_file)
-    return df
-
-def create_curr_df(log_path):
-    file = "CURR.csv"
-    location = log_path.parent
-    csv_file = os.path.join(location, file)
-    df = pd.read_csv(csv_file, index_col='timestamp')
-    df.index = pd.to_datetime(df.index, unit='s', origin='unix')
-    os.remove(csv_file)
-    return df 
-
-def create_err_df(log_path):
-    file = "ERR.csv"
+def create_df(log_path, csv_name):
+    file = csv_name + ".csv"
     location = log_path.parent
     csv_file = os.path.join(location, file)
     df = pd.read_csv(csv_file, index_col='timestamp')
@@ -68,16 +52,16 @@ def create_kml(kml_name):
     return kml_name
 
 def rgb_style(feature):
-    rgb_style = simplekml.LineStyle()
-    rgb_style.color = simplekml.Color.whitesmoke
-    rgb_style.width = 2.0
-    feature.linestyle = rgb_style
+    rgb_style = simplekml.Style()
+    rgb_style.linestyle.color = simplekml.Color.whitesmoke
+    rgb_style.linestyle.width = 2.0
+    feature.style = rgb_style
 
 def agr_style(feature):
-    agr_style = simplekml.LineStyle()
-    agr_style.color = simplekml.Color.red
-    agr_style.width = 2.0
-    feature.linestyle = agr_style
+    agr_style = simplekml.Style()
+    agr_style.linestyle.color = simplekml.Color.red
+    agr_style.linestyle.width = 2.0
+    feature.style = agr_style
 
 def create_linestring(log_path, kml, container_index):
     ls = kml.containers[container_index].newlinestring(name = log_path.name)
@@ -87,18 +71,35 @@ def create_linestring(log_path, kml, container_index):
     ls.coords = coords_list
     return ls
 
+def create_balloon_report(feature):
+    flight_time = ev_df.index[-1] - ev_df.index[0]
+    feature.balloonstyle.text = "Flight time: " + str(flight_time.components.minutes) + "m " + str(flight_time.components.seconds) + "s"
+    #feature.balloonstyle.bgcolor = simplekml.Color.lightgreen
+    #feature.balloonstyle.textcolor = simplekml.Color.rgb(0, 0, 255)
+    
+# def create_logbook(log_path):
+#     lb_dict = {"log":[], "bat_bto":[], "bat_ato":[], "bat_amp":[], "bat_res":[], "bat_ald":[], "to_time":[], "ld_time":[]}
+#     lb_dict["log"].append(log_path.name)
+#     lb_dict["bat_bto"].append(bat_df.columns)
+
 #running functions...
 log_list = create_log_path(path) 
+
 flights_kml = create_kml('flights_kml')
+
 for i in tqdm(log_list):
     create_csv(i)    
-    cam_df = create_cam_df(i)
+    cam_df = create_df(i, "CAM")
+    ev_df = create_df(i, "EV")
+    bat_df = create_df(i, "BAT")
     if any("90_rgb" in s for s in i.parts):
         rgb = create_linestring(i, flights_kml, 0)
         rgb_style(rgb)
+        create_balloon_report(rgb)
     elif any("120_agr" in s for s in i.parts):
         agr = create_linestring(i, flights_kml, 1)
         agr_style(agr)
+        create_balloon_report(agr)
     else:
         print("Invalid folder name.")
 
